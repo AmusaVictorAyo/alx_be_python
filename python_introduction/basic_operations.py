6a77cd95e420500d72a8edff0bb01f79d4c2565f
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-01-07 12:34:45 -0500

    My first ALX Python Project

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Add_num = number1 + number2
+Sub_num = number1 - number2
+Multiply_num = number1 * number2
+print(f"Addition of 10 and 5 is {Add_num}")
+print(f"Subtraction of 10 and 5 is {Sub_num}")
+print(f"Multiplication of 10 and 5 is {Multiply_num}")

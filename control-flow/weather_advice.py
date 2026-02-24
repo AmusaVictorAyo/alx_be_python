2403f9aec49bf3797cedc4e124b35409c251307d
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-02-24 10:17:44 -0500

    Built a weather advisor like app

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-request = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+request = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if request == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif request == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif request == "cold": 
+elif request == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")

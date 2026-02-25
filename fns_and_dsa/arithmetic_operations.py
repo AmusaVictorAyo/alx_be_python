8c96c9a9a41f6dce38e1d8bb83048fbb9fcfc7ac
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-02-25 09:16:47 -0500

    Performing multiple operations by choice

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# Defining a function that performs arithmetic operations based on the given operation type
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        # Handling Zero division error for division operands
+        case 'divide':
+            if num2 == 0:
+                return "Cannot divide by zero"
+            return num1 / num2
+        case _:
+            return "Invalid input"
